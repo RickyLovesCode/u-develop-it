d8f8805d6229ca52859ed2591572a528bb8816c9
Ricky27 <Rickyrenteria7@gmail.com>
2022-03-14 02:31:24 -0700

    connected db to server/ created api endpoints

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 const express = require('express');
-const res = require('express/lib/response');
+
 const PORT = process.env.PORT || 3002;
 const app = express();
 
@@ -25,34 +26,88 @@ const db = mysql.createConnection({
 // //     console.log(rows);
 // // });
 
-// //QUERY TO GET A SINGLE DATA 'CANDIDATE'
-// db.query(`SELECT * FROM candidates WHERE id = 9`, (err, row) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(row);
-// });
+// GET ALL CANDIDATES
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'sucess',
+            data: rows
+        });
+    });
+});
+
+//QUERY TO GET A SINGLE DATA 'CANDIDATE'
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
 
 // //QUERY TO DELETE DATA 'candidates'
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id= ?`;
+    const params = [req.params.id];
 
-// //QUERY TO CREATE DATA 'candidates'
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-// VALUES (?,?,?,?)`;
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.statusMessage(400).json({ error: message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found!'
+            });
+        } else {
+            res.json({
+                message: 'delete',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
 
-// const params = [1, 'Ronald', 'Firbank', 1];
+// CREATE A CANDIDATE
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body,
+        'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
 
-// db.query(sql, params, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
+    // //QUERY TO CREATE DATA 'candidates'
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
 
 //RES FOR USER REQUEST THAT ARE NOT SUPPORTED (ERR)
 app.use((req, res) => {
